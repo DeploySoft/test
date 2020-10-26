b32659182b350c0779bcc23e44253a2d8425583a
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-10-26 15:05:26 -0500

    Removed

---
+++ src/test/java/com/deploysoft/application/domain/usecase/impl/TransferDelegateImplTest.java
@@ -0,0 +1,15 @@
+package com.deploysoft.application.domain.usecase.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author : J. Andres Boyaca (janbs)
+ * @since : 6/10/20
+ **/
+class TransferDelegateImplTest {
+
+
+
+    public TransferDelegateImplTest() {
+    }
+}
